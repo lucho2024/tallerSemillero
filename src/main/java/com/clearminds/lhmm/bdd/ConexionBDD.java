0bc0e3ce5774b16351fa7d7476fd912c02e9c598
luis <luishm2024@gmail.com>
2020-12-09 09:24:49 -0500

    añadiendo properties

---
+++ src/main/java/com/clearminds/lhmm/bdd/ConexionBDD.java
@@ -0,0 +1,40 @@
+package com.clearminds.lhmm.bdd;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConexionBDD {
+
+	public static String leerPropiedad(String propiedad) {
+
+		Properties p = new Properties();
+		try {
+			File f = new File("conexion.properties");
+			p.load(new FileReader(f));
+
+			if (p.getProperty(propiedad) != null) {
+				return p.getProperty(propiedad);
+			} else {
+				return null;
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(leerPropiedad("propiedad3"));
+	}
+
+}
